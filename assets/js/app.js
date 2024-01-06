829c547179d7c3765ced09c9ff6131dbe5bd5095
Alfaraby Indra <alfarabyindra@gmail.com>
2024-01-06 21:56:39 +0700

    upload

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+//alpine.data global function
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Robusta Brazil", img: "1.jpg", price: 20000 },
+      { id: 2, name: "Arabica Blend", img: "2.jpg", price: 25000 },
+      { id: 3, name: "Aceh Gayo", img: "3.jpg", price: 40000 },
+      { id: 4, name: "Primo Passo", img: "4.jpg", price: 30000 },
+      { id: 5, name: "Sumatra Mandailing", img: "5.jpg", price: 35000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      this.items.push(newItem);
+      this.quantity++;
+      this.total += newItem.price;
+      console.log(this.total);
+    },
+  });
+});
+
+//konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
